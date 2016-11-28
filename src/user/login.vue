8cb08404f3c3c8e1c786f5378559e00ce6aa2305
codedreamfy@outlook.com <codedreamfy@outlook.com>
2016-11-28 23:49:43 +0800

    upt

--- src/user/login.vue
+++ src/user/login.vue
@@ -2,6 +2,8 @@
   <div class="from-group">
     <mt-field label="手机号" placeholder="请输入手机号" type="tel"></mt-field>
     <mt-field label="密码" placeholder="请输入密码" type="password"></mt-field>
+    <mt-button size="large" type="primary" >注册</mt-button>
+    <a class="tips-for-login" @click="turnForRegister">没有账号？立即注册</a>
   </div>
 </template>
 
@@ -11,6 +13,11 @@ export default {
     return {
       tel: '',
       password: ''
+    }
+  },
+  methods: {
+    turnForRegister () {
+      this.$emit('turn', {reg:true,login:false})
     }
   }
 }
