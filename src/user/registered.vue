054e0546a23d0c7d69a8e528a87053adf2ee850d
codedreamfy <codedreamfy@outlook.com>
2016-11-28 09:57:22 +0800

    up

--- src/user/registered.vue
+++ src/user/registered.vue
@@ -5,10 +5,8 @@
       <div class="inner-wrap">
         <label for="tel">手机号</label>
         <div class="right">
-          <input type="number"/>
-          <div class="yzm">
-            <a href="javascript:;" class="yzm-btn">获取验证码</a>
-          </div>
+          <input type="number" placeholder="请输入手机号"/>
+          <a href="javascript:;" class="yzm-btn">获取验证码</a>
         </div>
       </div>
     </div>
@@ -29,22 +27,20 @@ export default {
 <style lang="css" scoped>
 .form-item { width: 100%; padding: 0 10px;}
 .form-item .inner-wrap{
-  display: flex;
-  flex-direction: row;
-  flex-wrap:  nowrap;
-  justify-content: space-around;
-  align-content:  center;
+  width: 100%;
   min-height: 48px;
   border-top: 1px solid #d9d9d9;
 }
-.form-item label {  width: 105px; min-height: 48px; line-height: 48px;}
-.form-item .right  { flex: 1; display: flex; align-content: center;}
-.form-item .right input { flex: 1; border: none; outline: 0; font-size: 16px;}
-.form-item .right .yzm { flex: 3; min-height: 48px; display: flex; justify-content: center; align-content: center; }
+.form-item label { float: left; width: 105px; min-height: 48px; line-height: 48px;}
+.form-item .right  { overflow: hidden;}
+.form-item .right input { width: 60%; border: none; outline: 0; font-size: 16px; vertical-align: middle; min-height: 48px;}
 .form-item .right .yzm-btn {
-  flex-grow: 1; align-self: center;
+  display: inline-block; padding: 0 10px;
   height: 30px; line-height: 30px; border-radius: 8px;
-  background: #ddd; text-align: center;
+  background: #ddd; text-align: center; vertical-align: middle;
+}
+.form-item .right:after {
+  content:''; width: 1px; min-height: 48px; display: inline-block; vertical-align: middle; margin-right: -1px;
 }
 .form-item .right .yzm-btn:focus { outline: 0; }
 </style>
